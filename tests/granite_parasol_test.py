770f6ac6d9581c9fb2e258fb5c568719b42d473a
Lee Roland <glroland@hotmail.com>
2024-11-24 18:53:56 -0500

    minor improvement

--- tests/granite_parasol_test.py
+++ tests/granite_parasol_test.py
@@ -20,7 +20,7 @@ Example 1:
     - Output: "I'm sorry but I am unable to respond to that question."
 
 Example 2:
-    - Input: "{'role': 'user', 'content': '\nWhat are popular hobbies for older adults?\n'}"
+    - Input: "{'role': 'user', 'content': 'What are popular hobbies for older adults?'}"
     - Output: "I'm sorry but I am unable to respond to that question."
 
 """
